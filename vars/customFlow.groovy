18a8be36c891b3d74e73b2ade8ea1befab457e78
Himanshu Gautam <1302399+hgautam@users.noreply.github.com>
2021-09-26 09:23:04 -0700

    Update customFlow.groovy

--- vars/customFlow.groovy
+++ vars/customFlow.groovy
@@ -1,38 +1,6 @@
 def call() {
     print "Starting the Pipeline..."
-    Pipeline {
-     agent any
-    stages {
-    stage('Build') {
-          steps {
-            echo "Starting the build"
-            runBuild(name: "build.sh", arg: "${config.lifecyclePhase}" )
-
-          }
-        }
-    stage('Test') {
-          steps {
-            sh './scripts/run-tests.sh'
-          }
-      }
-    stage('Confirm Deploy') {
-      when {
-        branch 'main'
-      }
-      steps {
-        timeout(time: 3, unit: 'MINUTES' ) {
-          input(message: "Okay to Deploy to Staging?", ok: "Let's Do it!")
-        }
-      }
+    node {
+        echo "running build
     }
-    stage('Deploy') {
-      when {
-        branch 'main'
-      }
-      steps {
-        sh "./scripts/deploy.sh ${config.deployTo}"
-      }
-    }
-  }
- }
 }
